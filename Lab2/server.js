e9655392aa369c6e19ea2edd3be0627a5bd6cc93
emma-landry7 <emmablandry730@gmail.com>
2024-02-29 21:59:17 -0600

    slight formatting

--- Lab2/server.js
+++ Lab2/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-// parse requests of content-type - application / json;
 
 let dbConnect = require('./dbConnect');
 let userRoutes = require('./routes/userRoutes');
 let postRoutes = require('./routes/postRoutes');
 let commentRoutes = require('./routes/commentRoutes');
 
+// parse requests of content-type - application / json
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
